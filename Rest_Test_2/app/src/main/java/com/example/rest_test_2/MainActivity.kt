71b19c7f7334efcf5bca9e88f5ffa02b1adea4c5
tireks <leonru007@gmail.com>
2022-11-15 12:50:28 +0400

    done rest api test project

--- Rest_Test_2/app/src/main/java/com/example/rest_test_2/MainActivity.kt
+++ Rest_Test_2/app/src/main/java/com/example/rest_test_2/MainActivity.kt
@@ -2,10 +2,45 @@ package com.example.rest_test_2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.rest_test_2.databinding.ActivityMainBinding
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding;
+    private var disposable: Disposable? = null
+
+    private val wikiApiServe by lazy {
+        WikiApiService.create()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSearch.setOnClickListener {
+            if (binding.editSearch.text.toString().isNotEmpty()) {
+                beginSearch(binding.editSearch.text.toString())
+            }
+        }
+    }
+
+    private fun beginSearch(searchString: String) {
+        disposable = wikiApiServe.hitCountCheck("query", "json", "search", searchString)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                { result -> binding.txtSearchResult.text = "${result.query.searchinfo.totalhits} result found" },
+                { error -> Toast.makeText(this, error.message, Toast.LENGTH_SHORT).show() }
+            )
+    }
+
+    override fun onPause() {
+        super.onPause()
+        disposable?.dispose()
     }
 }
