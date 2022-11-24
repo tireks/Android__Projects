940f741003843b154be06fccad5764f8bae00038
tireks <leonru007@gmail.com>
2022-11-24 16:04:22 +0400

    improved popUp
    now it's ignoring tapping on background and closes only on button

--- Calc_new/app/src/main/java/com/tirex_projs/Calc_new/MainActivity.kt
+++ Calc_new/app/src/main/java/com/tirex_projs/Calc_new/MainActivity.kt
@@ -1,26 +1,31 @@
 package com.tirex_projs.Calc_new
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import android.view.Gravity
 import android.view.View
+import android.view.WindowManager
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.PopupWindow
-import com.tirex_projs.Calc_new.databinding.ActivityMainBinding;
+import androidx.appcompat.app.AppCompatActivity
+import com.tirex_projs.Calc_new.databinding.ActivityMainBinding
 import com.tirex_projs.Calc_new.databinding.PopupWindowBinding
 
-class MainActivity : AppCompatActivity(){
+
+class MainActivity : AppCompatActivity(),  View.OnClickListener {
     private lateinit var binding: ActivityMainBinding;
     private lateinit var bindingPop: PopupWindowBinding;
     private var bracketsCounter: Int = 0;
     private var blocker: Blocker = Blocker();
     private var cheker: Cheker = Cheker();
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         blocker.startInputBlock(binding)
+        binding.equButton.setOnClickListener(this)
     }
 
     fun opButtonOnClick(view: View){
@@ -33,6 +38,7 @@ class MainActivity : AppCompatActivity(){
         }
 
     }
+
     fun numberButtonOnClick(view: View){
         if (!blocker.isBlocked(view)) {
             blocker.smartBlocker(view, binding)
@@ -40,15 +46,9 @@ class MainActivity : AppCompatActivity(){
             if (view is Button) {
                 binding.TVInput.append(view.text)
             }
-            /*when (view) {
-                binding.dotButton -> {
-                    //if (!blocker.isBlocked(view)){
-                    blocker.smartBlocker(view, binding)
-                    //}
-                }
-            }*/
         }
     }
+
     fun acButtonOnClick(view: View){
         if (view is Button){
             binding.TVInput.text = ""
@@ -72,30 +72,54 @@ class MainActivity : AppCompatActivity(){
         }
 
     }
-    fun equButtonOnClick(view: View){
-        when (cheker.fullCheck(binding.TVInput.text.toString())){
-            0 -> {
+
+    private fun PopupWindow.dimBehind() {
+        val container = contentView.rootView
+        val context = contentView.context
+        val wm = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        val p = container.layoutParams as WindowManager.LayoutParams
+        p.flags = p.flags or WindowManager.LayoutParams.FLAG_DIM_BEHIND
+        p.dimAmount = 0.9f
+        wm.updateViewLayout(container, p)
+    }
+
+    private fun showPopUp(warnString: String, view: View) : PopupWindow{
+        bindingPop = PopupWindowBinding.inflate(layoutInflater)
+        bindingPop.myTV.text = warnString;
+        val wid = LinearLayout.LayoutParams.WRAP_CONTENT
+        val high = LinearLayout.LayoutParams.WRAP_CONTENT
+        val focus = true
+        val popupWindow = PopupWindow(bindingPop.root, wid, high, false)
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0,0)
+        popupWindow.dimBehind()
+        popupWindow.isOutsideTouchable = false;
+        popupWindow.isTouchable = true;
+        bindingPop.OkButton.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                popupWindow.dismiss()
+            }
+        })
+        return popupWindow
+    }
+
+    override fun onClick(view: View) {
+        var popupWindow : PopupWindow = PopupWindow()
+        if (!blocker.isBlocked(view)){
+            when (val errorCode = cheker.fullCheck(binding.TVInput.text.toString())){
+                0 -> {
                 //all is ok
+                }
+                1,2 -> {
+                    var warnString = ""
+                    warnString = if (errorCode == 1){
+                        "sorry, the quantity '(' and ')' doesn't seem to match"
+                    }else {
+                        "sorry, it seems the expression contains unnecessary brackets"
+                    }
+                    popupWindow = showPopUp(warnString, view)
+                }
             }
-            1 -> {
-                bindingPop = PopupWindowBinding.inflate(layoutInflater)
-                bindingPop.myTV.text = "sorry, the quantity '(' and ')' doesn't seem to match";
-                val wid = LinearLayout.LayoutParams.WRAP_CONTENT
-                val high = LinearLayout.LayoutParams.WRAP_CONTENT
-                val focus = true
-                val popupWindow = PopupWindow(bindingPop.root, wid, high, focus)
-                popupWindow.showAtLocation(view, Gravity.CENTER, 0,0)
-                //open modal window
-            }
-            2 -> {
-                bindingPop = PopupWindowBinding.inflate(layoutInflater)
-                bindingPop.myTV.text = "sorry, it seems the expression contains unnecessary brackets";
-                val wid = LinearLayout.LayoutParams.WRAP_CONTENT
-                val high = LinearLayout.LayoutParams.WRAP_CONTENT
-                val focus = true
-                val popupWindow = PopupWindow(bindingPop.root, wid, high, focus)
-                popupWindow.showAtLocation(view, Gravity.CENTER, 0,0)
-            }
+
         }
     }
 
