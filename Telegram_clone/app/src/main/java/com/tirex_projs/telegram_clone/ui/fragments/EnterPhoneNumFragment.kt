181ec2b91269d932db0e2aa90695739853fb0942
tireks <leonru007@gmail.com>
2022-12-16 13:54:15 +0400

    made actionMenu in settings

---
+++ Telegram_clone/app/src/main/java/com/tirex_projs/telegram_clone/ui/fragments/EnterPhoneNumFragment.kt
@@ -0,0 +1,12 @@
+package com.tirex_projs.telegram_clone.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.tirex_projs.telegram_clone.R
+
+class EnterPhoneNumFragment : Fragment(R.layout.fragment_enter_phone_num) {
+
+}
